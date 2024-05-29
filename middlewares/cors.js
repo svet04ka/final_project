55e02f487947a7da8fcb7234c12c16e030aa1d24
svet04ka <105312628+svet04ka@users.noreply.github.com>
2024-05-29 16:43:25 +0300

    обновление

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,18 +1,14 @@
+const allowedCors = ["https://svet04kapindie.nomorepartiesco.ru"];
+
 function cors(req, res, next) {
-    const { origin } = req.headers;
-    
-    if (allowedCors.includes(origin)) { // Если это наш друг
-        res.header('Access-Control-Allow-Origin', origin);
-    }
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
-    next();
+  const { origin } = req.headers;
+
+  if (allowedCors.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+  next();
 }
 
-const allowedCors = [
-    'https://practicum.yandex.ru',
-    'https://students-projects.ru',
-    'http://localhost:3000'
-  ]; 
-
-  module.exports = cors;
+module.exports = cors; 
